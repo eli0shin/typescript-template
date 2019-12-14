84cffd66f13ba5f8d3a6063dda88843123b80963
Eli Oshinsky <elimelech.oshinsky@gmail.com>
2019-12-14 13:48:30 -0500

    husky parallel

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ export function sum(a: number, b: number): number {
 export function subtract(a: number, b: number): number {
   return a - b;
 }
-export function random(): number {
-  return Number((Math.random() * 100000000).toFixed());
+export function random(numberOfDigits: number = 1): number {
+  return Number((Math.random() * numberOfDigits).toFixed());
 }
